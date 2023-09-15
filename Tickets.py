b2ffc1038245e549839c953043abf437749a7e9d
yohanhewage <84899009+yohanhewage@users.noreply.github.com>
2023-09-15 19:00:32 +1200

    Update Tickets.py

--- Tickets.py
+++ Tickets.py
@@ -7,7 +7,7 @@ class Ticket:
         Ticket.count += 1
         self.Ticket_creator = Ticket_creator
         self.Staff_id = Staff_id
-        self.Email = Email
+        self.Email = Email_address
         self.Description = Description
         self.Response = Response
         self.Ticket_status = Ticket_status
@@ -18,3 +18,13 @@ class Ticket:
         self.Status = True
         Ticket.tickets_resolved += 1
         Ticket.tickets_to_solve -= 1
+
+    def display_ticket(self):
+        # ------->>>>>> print ticket info<<<<<<<<----------
+        print(f"Ticket Number:, {self.Ticket_number}\n"
+              f"Ticket Creator: {self.Ticket_creator}\n"
+              f"Staff ID:, {self.Staff_id}\n"
+              f"Email Address:, {self.Email_address}\n"
+              f"Description:, {self.Description}\n"
+              f"Response:, {self.Response}\n"
+              f"Ticket Status:, {self.Status}\n")
